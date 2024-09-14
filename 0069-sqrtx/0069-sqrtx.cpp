bf73134b3d799f79190b3409172d08502b423ff2
Kartikey Keshari <kesharikartikey9140@gmail.com>
2024-09-14 22:56:43 +0530

    Time: 2 ms (51.72%), Space: 8.4 MB (19.89%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long low = 1;
+        long long high = x;
+        while(low <= high){
+            long long mid = (high+low)/2;
+            long long val = mid*mid;
+            if(val <= (long long)(x)){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return high;
+    }
+};
